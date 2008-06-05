f0e793c1fe6ddf42554cdd48353b4dacaaf250e6
juraj_slk <juraj_slk@735d13b6-9817-0410-8766-e36946ffe9aa>
2008-06-05 13:55:05 +0000

    initial upload
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/css_filelinks/trunk@9861 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_emconf.php
@@ -0,0 +1,48 @@
+<?php
+
+########################################################################
+# Extension Manager/Repository config file for ext: "css_filelinks"
+#
+# Auto generated 04-03-2008 11:10
+#
+# Manual updates:
+# Only the data in the array - anything else is removed by next write.
+# "version" and "dependencies" must not be touched!
+########################################################################
+
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'CSS styled Filelinks',
+	'description' => 'CSS based, XHTML and accessibility compliant implementation of the "FILELINKS" cObject.',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '0.2.14',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 0,
+	'createDirs' => '',
+	'modify_tables' => '',
+	'clearcacheonload' => 0,
+	'lockType' => '',
+	'author' => 'Juraj Sulek',
+	'author_email' => 'juraj@sulek.sk',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'php' => '3.0.0-0.0.0',
+			'typo3' => '3.5.0-0.0.0',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'_md5_values_when_last_written' => 'a:10:{s:9:"ChangeLog";s:4:"4ee4";s:10:"README.txt";s:4:"ee2d";s:25:"class.tx_cssfilelinks.php";s:4:"dd7d";s:21:"ext_conf_template.txt";s:4:"5313";s:12:"ext_icon.gif";s:4:"e533";s:12:"ext_icon.psd";s:4:"1422";s:17:"ext_localconf.php";s:4:"998b";s:14:"doc/manual.sxw";s:4:"ac2a";s:19:"doc/wizard_form.dat";s:4:"8064";s:20:"doc/wizard_form.html";s:4:"6ac0";}',
+);
+
+?>
